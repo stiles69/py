3965c419927cc4aba480df3f720aeb9f3193dec0
stiles69 <brett.salemink@gmail.com>
2019-01-10 00:20:38 -0600

    manjaro Bash

--- Send-Kodi-Message.py
+++ Send-Kodi-Message.py
@@ -37,7 +37,7 @@ port = '8080'
 # The username on XBMC's web interface (just comment or delete this line if you don't use authentication
 #username = 'xbmc'
 # Same as the username. No password for my system.
-password = ''
+#password = ''
 #
 json_name =  back_slash + "jsonrpc"
 full_http = "http:" + back_slash + back_slash + ip + colon + port + json_name
@@ -70,36 +70,36 @@ dot = "."
 iplist_file = 'IPList.txt'
 # constants end
 arguments = len(sys.argv)
-#print ("Parameters received = ",arguments)
+print ("Parameters received = ",arguments)
 #argument [0] is the name of the Python program being executed.
 #argument [1] is the parameter being passed to the Python program as the message
 #argument [2] is the parameter being passed to the Python program as the title
 title_text = "Comskip Completed."
-if arguments >= 2 :
-         title_text = sys.argv[1]
-         if arguments >= 3:
-                 recorded_program = sys.argv[2]
-                 if arguments >= 4:
-                          ip = sys.argv[3]
-                          if arguments >= 3:
-                                   port = sys.argv[4]
-         json_name =  back_slash + "jsonrpc"
-         full_http = "http:" + back_slash + back_slash + ip + colon + port + json_name
-         xbmc = XBMC(full_http)
-if arguments == 1 :
-        recorded_program = sys.argv[0]
-str1 = recorded_program
-str2 = str1.rpartition(slash)[2]
-str3 = str2.partition(dot)[0]
-if debug_it:
-         print "str1=",str1, "str2=",str2, "str3=",str3
-str4 = str3.partition(underscore)
-if debug_it:
-         print "str4= ",str4
-recorded_program = str4[0]
-if debug_it:
-         print "str4 =", str4, "recorded program = ",recorded_program
-parameters = {'title': title_text,'message': recorded_program}
+#if arguments >= 2 :
+#         title_text = sys.argv[1]
+#         if arguments >= 3:
+#                 recorded_program = sys.argv[2]
+#                 if arguments >= 4:
+#                          ip = sys.argv[3]
+#                          if arguments >= 3:
+#                                   port = sys.argv[4]
+#         json_name =  back_slash + "jsonrpc"
+#         full_http = "http:" + back_slash + back_slash + ip + colon + port + json_name
+#         xbmc = XBMC(full_http)
+#if arguments == 1 :
+#        recorded_program = sys.argv[0]
+#str1 = recorded_program
+#str2 = str1.rpartition(slash)[2]
+#str3 = str2.partition(dot)[0]
+#if debug_it:
+#         print "str1=",str1, "str2=",str2, "str3=",str3
+#str4 = str3.partition(underscore)
+#if debug_it:
+#         print "str4= ",str4
+#recorded_program = str4[0]
+#if debug_it:
+#         print "str4 =", str4, "recorded program = ",recorded_program
+#parameters = {'title': title_text,'message': recorded_program}
 
 # This is a single, reusable method that makes a call to XBMC and gives you back the response
 
@@ -109,9 +109,9 @@ def sendJson(url_in,method,parameters):
         temp_str = url_in
         xbmc = XBMC(temp_str)
         if method == notify_method:
-                #xbmc.method(parameters)
+                xbmc.method(parameters)
                 results = xbmc.GUI.ShowNotification(parameters)
-                #title="Library Clean", message = "Initiated")
+                title="Library Clean", message = "Initiated")
         else:
                 if method == libscan_method:
                         if "win" in sys.platform:
@@ -130,7 +130,7 @@ def sendJson(url_in,method,parameters):
 #
 
 # Here's an example of using the above method and variable values to make XBMC show a Notify
-results =sendJson(full_http,method,parameters)
+results =sendJson(full_http,notify_method,parameters)
 time.sleep(2)
 # I just print the results out
 print "results= ", results, " from ", full_http
